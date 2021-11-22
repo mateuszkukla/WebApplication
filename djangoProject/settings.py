19fb9f05cdc7c5454e44f098bef64ec9ebcff988
Arkadiusz Cieply <152081@stud.prz.edu.pl>
2021-11-22 01:52:24 +0100

    Dodanie crispy tag do rejestracji

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -131,3 +132,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
